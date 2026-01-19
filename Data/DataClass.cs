90fe0b0ab009230806fff87d05a8a936766b441a
Karor9 <58850040+Karor9@users.noreply.github.com>
2026-01-19 23:46:22 +0100

    book data

---
+++ Data/DataClass.cs
@@ -0,0 +1,23 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgressApp.Data;
+
+public partial class DataClass : ObservableObject
+{
+    [ObservableProperty]
+    string fieldName;
+    [ObservableProperty]
+    int currentChapter;
+    [ObservableProperty]
+    int maxChapter;
+
+    public DataClass(string fieldName, int currentChapter, int maxChapter)
+    {
+        FieldName = fieldName;
+        CurrentChapter = currentChapter;
+        MaxChapter = maxChapter;
+    }
+}
--- ViewModel/MainViewModel.cs
+++ ViewModel/MainViewModel.cs
@@ -5,36 +5,68 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Text;
+using System.Text.Json;
+using ProgressApp.Data;
 
 namespace ProgressApp.ViewModel;
 
 public partial class MainViewModel : ObservableObject
 {
+    private readonly string filePath;
     public MainViewModel()
     {
-        items = new ObservableCollection<string>();
+        filePath = Path.Combine(FileSystem.AppDataDirectory, "item.json");
+        Items = new ObservableCollection<DataClass>();
+        //LoadItems();
     }
+
     [ObservableProperty]
-    ObservableCollection<string> items;
+    ObservableCollection<DataClass> items;
 
     [ObservableProperty]
     string text;
+    [ObservableProperty]
+    int currentChapter;
+    [ObservableProperty]
+    int totalChapter;
 
     [RelayCommand]
     void Add()
     {
         if (string.IsNullOrWhiteSpace(Text))
             return;
-        Items.Add(Text);
+        Items.Add(new DataClass(Text, currentChapter, totalChapter));
         Text = string.Empty;
+        SaveItems();
     }
 
     [RelayCommand]
-    void Delete(string s)
+    void Delete(DataClass s)
     {
         if(Items.Contains(s))
         { 
-            Items.Remove(s); 
+            Items.Remove(s);
+            SaveItems();
         }
+    }
+
+    private void LoadItems()
+    {
+        if (!File.Exists(filePath))
+            return;
+
+        var json = File.ReadAllText(filePath);
+        var loader = JsonSerializer.Deserialize<ObservableCollection<DataClass>>(json);
+
+        if (loader is null)
+            return;
+
+        Items = loader;
+    }
+
+    private void SaveItems()
+    {
+        var json = JsonSerializer.Serialize(Items);
+        File.WriteAllText(filePath, json);
     }
 }
